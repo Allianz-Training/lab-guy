affe5567ca9b8bf1ef8c1e02e49128e550722030
paritguy <58096870+paritguy@users.noreply.github.com>
2020-12-16 16:30:20 +0700

    lab 16Dec

--- AngularLab/src/app/app.component.ts
+++ AngularLab/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { BackendService } from './backend.service';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
@@ -9,22 +10,24 @@ import { ProductListComponent } from './product-list/product-list.component';
 export class AppComponent implements AfterViewInit {
   @ViewChild('productList')
   productList: ProductListComponent;
+  constructor(private backendService: BackendService) {}
 
   ngAfterViewInit(): void {
-    this.productList.products = [
-      {
-        name: 'ส้มโอ',
-        price: 111,
-      },
-      {
-        name: 'แตงโม',
-        price: 222,
-      },
-      {
-        name: 'มะพร้าวน้ำหอม',
-        price: 333,
-      },
-    ];
+    this.productList.products = this.backendService.getProducts();
+    // this.productList.products = [
+    //   {
+    //     name: 'ส้มโอ',
+    //     price: 111,
+    //   },
+    //   {
+    //     name: 'แตงโม',
+    //     price: 222,
+    //   },
+    //   {
+    //     name: 'มะพร้าวน้ำหอม',
+    //     price: 333,
+    //   },
+    // ];
   }
 
   title = 'angular29-lab1';
--- AngularLab/src/app/app.module.ts
+++ AngularLab/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductItemComponent } from './product-item/product-item.component';
-import { PanelComponent } from './panel/panel.component';
+import { BackendService } from './backend.service';
+import { CatalogModule } from './catalog/catalog.module';
+import { HomeComponent } from './home/home.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+]
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductItemComponent, PanelComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule, CatalogModule, RouterModule.forRoot(routes)],
+  providers: [BackendService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
